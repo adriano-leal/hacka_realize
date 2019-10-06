9806da4c1b776e05bb1757dcb0db00edf8673c6a
Guilherme Rangel <gui.vg16@hotmail.com>
2019-10-06 10:21:49 -0300

    bigFix and create screen finish

---
+++ Realize_hackathon/Realize_hackathon/App/FinishCard/FinishCardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FinishCardViewController.swift
+//  Realize_hackathon
+//
+//  Created by Guilherme Rangel on 06/10/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+class FinishCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .black
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
