c390ab0bea868fb7d048f98de9bd4b5339029ad7
Guilherme Rangel <gui.vg16@hotmail.com>
2019-10-06 05:41:55 -0300

    create screen requestCard

---
+++ Realize_hackathon/Realize_hackathon/App/CardSelection/cardSelectionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  cardSelectionViewController.swift
+//  Realize_hackathon
+//
+//  Created by Guilherme Rangel on 06/10/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+class cardSelectionViewController: UIViewController {
+
+    @IBOutlet weak var viewCard: UIView!
+    
+    @IBOutlet weak var btnRequest: UIButton!
+    @IBAction func btnRequest(_ sender: UIButton) {
+        print("next")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewCard.layer.cornerRadius = 15
+        
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Ajuda", style: .done, target: self, action: nil)
+    }
+    
+
+   
+
+}
