16d7375b0572bed3da2ca683d2712a5e8673d076
Adriano Leal Ramos <ramos.adrianoleal@gmail.com>
2019-10-06 04:00:37 -0300

    project, login screen done

---
+++ Realize_hackathon/Realize_hackathon/App/Login/LoginViewController.swift
@@ -0,0 +1,84 @@
+//
+//  LoginViewController.swift
+//  Realize_hackathon
+//
+//  Created by Adriano Ramos on 06/10/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+import TextFieldEffects
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var cpfField: HoshiTextField! {
+        didSet {
+            cpfField.setCPFIcon(#imageLiteral(resourceName: "cpf"))
+        }
+    }
+    
+    @IBOutlet weak var labelsView: UIView!
+    @IBOutlet weak var lblCpf: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .black
+//        self.hideKeyboard()
+        cpfField.delegate = self
+
+        lblCpf.isHidden = false
+        cpfField.backgroundColor = .black
+        cpfField.textColor = .white
+        cpfField.borderInactiveColor = .lightGray
+        cpfField.borderActiveColor = .white
+        cpfField.tag = 25
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification , object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification , object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification , object: nil)
+
+        
+        cpfField.addTarget(self, action: #selector(hideCpf), for: .editingDidBegin)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func hideCpf() {
+        lblCpf.isHidden = true
+    }
+    
+    @objc func keyboardWillChange(notification: NotificationCenter) {
+        self.labelsView.frame.size.height = -60
+    }
+    
+    @objc func DismissKeyboard(){ view.endEditing(true) }
+    
+    func hideKeyboard() {
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+}
+
+
+extension UITextField {
+    
+    func setCPFIcon(_ image: UIImage) {
+        let cpfIcon = UIImageView(frame: CGRect(x: 10, y: 5, width: 23, height: 23))
+        cpfIcon.image = image
+        
+        let cpfViewContainer: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        cpfViewContainer.addSubview(cpfIcon)
+        leftView = cpfViewContainer
+        leftViewMode = .always
+    }
+    
+}
+
+
